7ddd00f5a2fb6522e9943c1b195fdd969be86401
Smallczyk <wojtas@strona.pl>
2021-12-11 17:10:26 +0100

    maven added

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import java.util.concurrent.TimeUnit;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Main {
 
@@ -29,6 +32,7 @@ public class Main {
         button3.click();
 
 
-        //*[@id="id_submitbutton"]
+
+
     }
 }
